4188a7c228cc93d020d33cdf2366dbb8f2b1e126
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-04-20 17:30:40 +0300

    Add square outlines, generate stars

--- gameScene.js
+++ gameScene.js
@@ -22,6 +22,11 @@ class GameScene extends Phaser.Scene {
       4: 0x1E1B46,
       5: 0x231F52
     }
+
+    gameState.canvasWidth = document.querySelector("canvas").offsetWidth;
+    gameState.canvasHeight = document.querySelector("canvas").offsetHeight;
+
+
     gameState.backgroundTiles = this.add.group();
     // const testRect = this.add.rectangle(0,0, 150, 150, 0xff0000)
     // gameState.backgroundTiles.add(testRect);
@@ -30,18 +35,46 @@ class GameScene extends Phaser.Scene {
       for (let j = 0; j < 600; j += 100) {
         const bgColor = gameState.bgColors[j / 100];
         const tile = this.add.rectangle(i, j, 100, 100, bgColor).setOrigin(0);
-        tile.set
+        tile.strokeColor = 0xa7f66c;
+        tile.isStroked = true;
+        tile.lineWidth = 2
+        tile.column = i / 100;
+        tile.row = j / 100;
         gameState.backgroundTiles.add(tile);
       }
     }
+
+    console.log(gameState.backgroundTiles);
     
     gameState.stars = this.add.group();
 
-    const testStar = this.add.image(100, 100, 'star').setScale(0.1);
-    gameState.stars.add(testStar);
 
+    // const testStar = this.add.image(100, 100, 'star').setScale(0.1);
+    // gameState.stars.add(testStar);
 
+    for (let i = 0; i < gameState.backgroundTiles.children.entries.length; i++) {
+      gameState.stars[i] = this.add.group();
+      for (let j = 0; j < 8; j++) {
+        const xOrigin = gameState.backgroundTiles.children.entries[i].x;
+        const yOrigin = gameState.backgroundTiles.children.entries[i].y;
+        const xCoord = xOrigin + Math.random() * 100;
+        const yCoord = yOrigin + Math.random() * 100;
+        const star = this.add.image(xCoord, yCoord, 'star').setScale(0.1);
+        gameState.stars[i].add(star);
+      }
+      // console.log(tile);
 
+    }
+
+    // console.log(gameState.backgroundTiles.children.entries[1]);
+    console.log(gameState.stars);
+
+    // for (let i = 0; i < 100; i++) {
+    //   const xCoord = Math.random() * (gameState.canvasWidth - 20) + 10;
+    //   const yCoord = Math.random() * (gameState.canvasHeight - 20) + 10;
+    //   const star = this.add.image(xCoord, yCoord, 'star').setScale(0.1);
+    //   gameState.stars.add(star);
+    // }
 
 		// gameState.player = this.physics.add.sprite(225, 450, 'codey').setScale(.5);
 
@@ -51,11 +84,11 @@ class GameScene extends Phaser.Scene {
 
 		// gameState.scoreText = this.add.text(195, 485, 'Score: 0', { fontSize: '15px', fill: '#000000' });
 
-		gameState.player.setCollideWorldBounds(true);
+		// gameState.player.setCollideWorldBounds(true);
 
-		this.physics.add.collider(gameState.player, platforms);
+		// this.physics.add.collider(gameState.player, platforms);
 
-		gameState.cursors = this.input.keyboard.createCursorKeys();
+		// gameState.cursors = this.input.keyboard.createCursorKeys();
 
 		// const bugs = this.physics.add.group();
 
@@ -96,12 +129,12 @@ class GameScene extends Phaser.Scene {
 	}
 
 	update() {
-		if (gameState.cursors.left.isDown) {
-			gameState.player.setVelocityX(-160);
-		} else if (gameState.cursors.right.isDown) {
-			gameState.player.setVelocityX(160);
-		} else {
-			gameState.player.setVelocityX(0);
-		}
+		// if (gameState.cursors.left.isDown) {
+		// 	gameState.player.setVelocityX(-160);
+		// } else if (gameState.cursors.right.isDown) {
+		// 	gameState.player.setVelocityX(160);
+		// } else {
+		// 	gameState.player.setVelocityX(0);
+		// }
 	}
 }
